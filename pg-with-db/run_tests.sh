ca3274ccd94527cf1862ccee50d56315c600d40c
chris jones <cjonesrun@gmail.com>
2019-10-15 21:17:35 -0400

    fixed test cases

--- pg-with-db/run_tests.sh
+++ pg-with-db/run_tests.sh
@@ -1,1 +1,11 @@
-exit 0;
+#!/bin/bash
+
+# this script is run when the docker container is built
+
+echo "*** RUNNING test.sh ***"
+
+psql widget_repo cj -c "select count(*) from widget;"
+
+echo "*** COMPLETE        ***"
+
+exit 1;
