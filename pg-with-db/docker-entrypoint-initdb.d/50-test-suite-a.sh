1e596d517ff60f111dc01580a0e00b3a23328384
chris jones <cjones@cbnco.com>
2019-10-16 16:10:04 -0400

    moved files around

--- pg-with-db/docker-entrypoint-initdb.d/50-test-suite-a.sh
+++ pg-with-db/docker-entrypoint-initdb.d/50-test-suite-a.sh
@@ -2,9 +2,9 @@
 
 # this script is run when the docker container is built
 
-echo "*** RUNNING test.sh ***"
+echo "******** RUNNING $0"
 
 psql widget_repo cj -c "select count(*) from widget;"
 
-echo "*** COMPLETE        ***"
+echo "******** COMPLETE $0"
 
